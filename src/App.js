e07f0793f571628262a6f594fac3c1479f9be305
DakuwoN <matthewleemccane@gmail.com>
2024-05-19 16:22:57 -0400

    Implemented PostCard with MUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'; 
 import Header from './components/Header/Header';
+import PostCard from './components/PostCard/PostCard';
 
 
 function App() {
   return (
     <div>
       <Header/>
-      
+      <PostCard title="Title" subreddit="r/subreddit" imageUrl="https://source.unsplash.com/random" content="Content"/>
     </div>
   );
 }
---
+++ src/components/PostCard/PostCard.js
@@ -0,0 +1,58 @@
+import { Card, CardContent, CardMedia, Typography, Divider, CardActions, IconButton } from '@mui/material';
+import { ArrowUpward, ArrowDownward, Comment } from '@mui/icons-material';
+
+function PostCard({ title, subreddit, imageUrl, content }) {
+  return (
+    <Card sx={{ 
+        display: 'flex',
+        flexDirection: 'column',
+        maxWidth: '75%',
+        marginBottom: 2,
+        height: '500px',
+        marginTop: 4,
+        boxShadow: 3,
+        '&:hover': {
+            border: '2px solid black',
+            borderColor: 'black',
+        }
+    }}>
+         
+      <CardContent sx={{flexGrow: 1}}>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography color="text.secondary">
+          {subreddit}
+        </Typography>
+      </CardContent>
+      <Divider sx={{borderColor: 'text.primary'}}/>
+      <CardMedia
+        component="img"
+        height="300"
+        image={imageUrl}
+        alt={title}
+        sx={{ width: 1}}
+      />
+      <Divider sx={{borderColor: 'text.primary'}}/>
+      <CardContent sx={{flexGrow: 1}}>
+        <Typography variant="body2">
+          {content}
+        </Typography>
+      </CardContent>
+      
+      <CardActions>
+        <IconButton aria-label="upvote" sx={{marginLeft: 1}}>
+          <ArrowUpward />
+        </IconButton>
+        <IconButton aria-label="downvote" sx={{marginRight: 79}}>
+          <ArrowDownward />
+        </IconButton>
+        <IconButton aria-label="comment">
+          <Comment />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default PostCard;
