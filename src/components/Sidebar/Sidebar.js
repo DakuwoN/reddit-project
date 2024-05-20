c38bc26406172ea60e981e2a43ccd54b10ebc7d6
DakuwoN <matthewleemccane@gmail.com>
2024-05-19 21:49:50 -0400

    Sidebar styling

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,19 @@
-import { Link } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
 
 function Sidebar({ subreddits }) {
   return (
-    <Box display="flex" flexDirection="column">
-      {subreddits && subreddits.map(subreddit => (
-        <Link key={subreddit} to={`/r/${subreddit}`}>{subreddit}</Link>
-      ))}
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', borderRadius: '10px', marginTop: 3, border: '1px solid black' }}>
+      <Typography variant="h6" component="div" sx={{ padding: '10px', textAlign: 'center' }}>
+        Subreddits
+      </Typography>
+      <List component="nav" aria-label="subreddits">
+        {subreddits && subreddits.map((subreddit) => (
+          <ListItem button divider key={subreddit} component={RouterLink} to={`/r/${subreddit}`}>
+            <ListItemText primary={subreddit} sx={{textAlign: 'center'}} />
+          </ListItem>
+        ))}
+      </List>
     </Box>
   );
 }
