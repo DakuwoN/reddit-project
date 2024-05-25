bb812b7c02e55772bd507e2421036a3c4e129d09
DakuwoN <matthewleemccane@gmail.com>
2024-05-24 23:36:42 -0400

    refactor: Update routing in App.js and Sidebar com ponent

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Grid } from '@mui/material';
 import Header from './components/Header/Header';
-import PostCard from './components/PostCard/PostCard';
 import Sidebar from './components/Sidebar/Sidebar';
 import SearchResultsPage from './pages/SearchResultsPage'; // Import SearchResultsPage
 import SubredditPage from './pages/SubredditPage';
